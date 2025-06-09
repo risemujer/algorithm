c6761c03ce89bd6ebd23e7f721f9aed62436e6b8
LEE SEUNG HEE <sooksh1125@sookmyung.ac.kr>
2025-06-09 15:45:55 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,10 +1,6 @@
+// reverse() : JS 내장 배열 메서드로 쉽게 뒤집음
+// slice() : 배열을 얕게 복사해서 원본을 손상하지 않게 처리
+
 function solution(num_list) {
-    let reverse_list = [];
-    const idx = num_list.length - 1;
-    
-    for(i=idx ; i >= 0 ; i--) {
-        reverse_list.push(num_list[i])
-    }
-    
-    return reverse_list;
+    return num_list.slice().reverse();
 }
