7cb4cbea4d1e407cc2c29876881db1288d9971cb
LEE SEUNG HEE <sooksh1125@sookmyung.ac.kr>
2025-06-09 19:18:22 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,17 +1,3 @@
 function solution(num_list) {
-    // 짝수와 홀수의 개수를 넣어 줄 변수 선언
-    let even = 0;
-    let odd = 0;
-    
-    for(let num of num_list) {
-        if (num % 2 === 0) {
-            even++;
-        }else {
-            odd++;
-        }
-    }
-    
-    return [even, odd];
-    
-    
+    return [num_list.filter(num => num % 2 === 0).length, num_list.filter(num=> num % 2 !== 0).length];
 }
