654f785bc34586067f8d6377f0e44fb87b708d6a
LEE SEUNG HEE <sooksh1125@sookmyung.ac.kr>
2025-06-09 18:28:03 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.js
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.js
@@ -1,7 +1,10 @@
 // split : 문자열을 문자 배열로 바꿈 
+// => split은 문자열만 사용 가능, 
+// => 스프레드 문법 : 문자열,배열,Set 모두 가능 & 유니코드 지원 
 // reverse : 배열을 뒤집음
 // join : 다시 문자열로 결합
 
 function solution(my_string) {
-    return my_string.split('').reverse().join('');
+    //return my_string.split('').reverse().join('');
+    return [...my_string].reverse().join('');
 }
